781674a7620dcee5173abda8fab2d994fc779581
Dragos Hont <dragos.hont@gmail.com>
2024-12-07 01:00:54 +0200

    list the drives before dd

--- create_windows_usb.sh
+++ create_windows_usb.sh
@@ -5,6 +5,10 @@
 ISO_URL="https://software.download.prss.microsoft.com/dbazure/Win11_24H2_EnglishInternational_x64.iso?t=4f3887d6-b42e-4634-aab6-801a9b6a62a4&P1=1733608031&P2=601&P3=2&P4=f4j88C%2bbrYklMWM85afasg3PZSySPij4yTAxQztHsEHQa6GBRE349KCDP7WbmIjLcjh4iOVg8lkm%2bX6PO3pl%2b2ADQR1jgdTnd5gyskEkxuLc07mwkxtHPggv4sU%2b3cUk%2bWloWn3yWW%2fzLlBqtxiefZMKyYYyZ9oT976J2wMksEs9AjClgoIFOLwdI4RvJp2HFnBf1db5TjlZUREXrB2ubYY74Nz6R3%2fMh7nx2rF1GndmgK1aYR%2fTo2R1NI0mVoD%2f%2f4VTW2CGDLt1%2fTq5FfkNIpP%2fZpgKLQcF9bdDQJGa8P9UikJOU1eW2DkLXrndrOho8ARP%2f%2bA9CfSfey%2fGKFwRnw%3d%3d"
 OUTPUT_ISO="windows11.iso"
 
+# List available drives
+echo "Listing available drives:"
+lsblk -o NAME,SIZE,TYPE,MOUNTPOINT
+
 # Prompt the user for the USB device path (e.g., /dev/sdX)
 read -p "Please enter the USB device path (e.g., /dev/sdX): " USB_DEVICE
 
